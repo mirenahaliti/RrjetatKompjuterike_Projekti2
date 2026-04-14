9d9bea7fe0e2b70ef21023c2fe12b764e0a30f8f
Mirena Haliti <mirena.haliti@student.uni-pr.edu>
2026-04-14 04:40:45 +0200

    konfigurimi fillestar i serverit

--- udpserver.py
+++ udpserver.py
@@ -5,33 +5,32 @@ import time
 import json
 from datetime import datetime
 
-
+# 1. KONFIGURIMET (Pika 1 e detyrës)
 IP = '192.168.0.24'
-PORT = 12000
+PORT = 12009
 HTTP_PORT = 8080
-MAX_CLIENTS = 5
+MAX_CLIENTS = 10  # Pragu (Pika 2)
 BUFFER_SIZE = 4096
 SERVER_DIR = "server_files"
 
 if not os.path.exists(SERVER_DIR):
     os.makedirs(SERVER_DIR)
 
+# Struktura për monitorim (Pika 4 dhe 7)
+stats = {
+    "active_connections": 0,
+    "total_messages": 0,
+    "clients_info": [],
+    "active_addrs": {}
+}
+
+admin_addr = None  # Për qasjen e plotë (Pika 6)
 
 
-
-
-def start_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind((IP, PORT))
-    server.listen(MAX_CLIENTS)
-    print(f"[*] Serveri ne portin {PORT}. Monitorimi: {HTTP_PORT}")
-    threading.Thread(target=http_monitor_server, daemon=True).start()
-
-
-if __name__ == "__main__":
-    start_server()
-
-
-
-
+def get_file_info(filename):
+    path = os.path.join(SERVER_DIR, filename)
+    if os.path.exists(path):
+        size = os.path.getsize(path)
+        ctime = datetime.fromtimestamp(os.path.getctime(path)).strftime('%Y-%m-%d %H:%M:%S')
+        return f"Size: {size} bytes, Created: {ctime}"
+    return "Skedari nuk u gjet."
